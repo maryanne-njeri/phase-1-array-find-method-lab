e054bb8407b402659ce7ed680528742d49b05626
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-14 14:16:39 +0300

    commit tree c8b733b0e6a5db92ab66718fa59cc3e1d2fc0cff

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const superbowlWin = (rec) => {
+    for(const myItem of rec){
+        if(myItem.result === "W"){
+            return myItem.year;
+        }
+    }
+        
+}
